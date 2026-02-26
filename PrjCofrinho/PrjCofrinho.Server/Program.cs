c4207602923b0ce240a6a03046fd1831f66db3f0
Bruno Ribeiro Batista <bruno@engsupport.eti.br>
2026-02-26 00:06:59 -0300

    Create ASP.NET Core/React solution app
    26/02/2026
    00:06

---
+++ PrjCofrinho/PrjCofrinho.Server/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire client integrations.
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapDefaultEndpoints();
+
+app.UseFileServer();
+
+app.Run();
+
+// Classes do banco de dados "traduzidas" para o backend
+public record Pessoas(int Id_Pessoa, String Nome, int Idade);
+public record Categorias(int Id_Categoria, String Descricao, int Finalidade);
+public record Transacoes(int Id_Transacao, String Descricao, int Valor, bool Tipo, int Id_Categoria, int Id_Pessoa);
